34f71bf8b22a8e34998bfbf132231824eee8a6d2
Diegous <duribe@cobralex.cl>
2017-10-13 17:53:28 -0300

    adding data to file

--- notes.js
+++ notes.js
@@ -1,8 +1,35 @@
 console.log( 'Starting notes.js' );
 
+const fs = require('fs');
+
 // add notes
 var addNote = (title, body) => {
-  console.log( 'Adding note', title, body);
+  // notes array
+  var notes = [];
+  // single note definition object
+  var note = {
+    title,
+    body
+  };
+
+  // validate if exist notes json file
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+
+  }
+
+  // validate if notes are duplicate by the title (filter return an array)
+  var duplicateNotes = notes.filter((note) => note.title === title );
+
+  if(duplicateNotes.length === 0){
+    // add new note to array of notes
+    notes.push(note);
+    // write new note to json file
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  };
+
 };
 
 // get all notes
--- playground/json.js
+++ playground/json.js
@@ -7,8 +7,29 @@
 // console.log( typeof stringObj );
 // console.log( stringObj );
 
-var personString = '{"name": "Diego", "age": 32}';
-var person = JSON.parse(personString);
+// var personString = '{"name": "Diego", "age": 32}';
+// var person = JSON.parse(personString);
+//
+// console.log( typeof person);
+// console.log( person );
 
-console.log( typeof person);
-console.log( `Person: ${person}`);
+const fs = require('fs');
+
+// writing file
+var originalNote = {
+  title: 'Some Title',
+  body: 'Some Body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+// originalNoteString
+fs.writeFileSync('notes.json', originalNoteString);
+
+// reading file
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString)
+
+// note
+console.log( typeof note);
+console.log( note.title );
